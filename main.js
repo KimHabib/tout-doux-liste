351e95abed35f8cf45dc7afbef240e89d986bc1b
kimshabib <kimshabib@gmail.com>
2021-03-17 19:51:54 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 
     // What is the user doing:
-    // 1. Typing a shopping item in the input box
-    // 2. Clicking on the Add Item button
+    // 1. Typing a to do list item in the input box
+    // 2. Clicking on the Add button
 
     // What must we do with JS:
 
     // WRITE WHAT FUNCTIONS EACH OF THESE STEPS REQUIRE
+
     // 1. listen for a button click from the user
-    var addItemBtn = document.querySelector('#add-item-btn')
+    var addItemBtn = document.querySelector('#submit-btn')
     
     // 4. find the list on the page
-    var shoppingList = document.querySelector('.shopping-list')
+    var toDoList = document.querySelector('#to-do-list')
+
     // creating empty shopping item array
-    var shoppingItems = []
+    var listItems = []
     addItemBtn.addEventListener('click', function (event) {
-      // 1. a) prevent the page refresh on click
+// prevent the page refresh on click
       event.preventDefault()
+
       // 2. find the item from the input
       // 3. store the item in a variable
-      var itemName = document.querySelector('.item')
+      var itemName = document.querySelector('.textField')
+
       // add item name to the array of shopping items
-      shoppingItems.push(itemName.value)
+      listItems.push(itemName.value)
+
+
+
       // 5. add item to the list
-      shoppingList.innerHTML += '<li>' + itemName.value + '</li>'
+      toDoList.innerHTML += '<li>' + itemName.value + '</li>'
       itemName.value = ''
     })
-    var sortButton = document.querySelector('#sort-btn')
-    sortButton.addEventListener('click', function (event) {
-      event.preventDefault()
-      // 1.) sort the array
-      shoppingItems.sort()
-      // 2.) display the contents of the array
-      // clear shopping list
-      shoppingList.innerHTML = ''
-      for (var i = 0; i < shoppingItems.length; i++) {
-        shoppingList.innerHTML += '<li>' + shoppingItems[i] + '</li>'
-      }
-    })
+
+    // play audio on button click
+    var audio = new Audio("https://soundcloud.com/nokia-design-team/cat");
+
+document.onclick = function() {
+  audio.play();
+}
+
+// cross off list item if the user completes the task and ticks the box
+// display all items if user clicks on "all"
+// display completed items if user clicks on "completed"
+// display active items if user clicks on "active"
+
+
