5f6aea21e71e43988f0173b9169657c8225097cc
Kim Habib <54062979+KimHabib@users.noreply.github.com>
2021-04-07 16:57:21 +0100

    column-reverse default view for list

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ function createItemAsListItem(key, { title, completed }) {
   li.append(checkbox, span);
   li.setAttribute("data-key", key);
   // adds an li to the to do list
-  toDoList.prependChild(li);
+  toDoList.appendChild(li);
 }
 
 function specialEffects() {
